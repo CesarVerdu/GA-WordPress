cec204230d26f9ef3dfd517245449436874291bf
Vichejo <vichejo@debian.dominio>
2022-02-24 20:18:58 +0100

    Comprobacion Final y Ayuda

--- analytics.php
+++ analytics.php
@@ -8,6 +8,23 @@ Author: Cesar Verdú
 License: GPLv3
 */
 
+//Agregar la funcionalidad a web en el header
+add_action( 'wp_head', 'agregar_ga' );
+function agregar_ga() {
+    echo "
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
+        i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+        a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
+        m.parentNode.insertBefore(a,m)})(window,document,'script',
+        'https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-0000000-1', 'auto');
+        ga('send', 'pageview');
+    </script>";
+ }
+
 //Activacion del pluggin con valores por defecto
 register_activation_hook( __FILE__, 'cv_set_default_options' );
 
@@ -49,16 +66,43 @@ function cv_ayuda() {
 
 function cv_ayuda_instruciones() {
     ?>
-    <p>Aqui colocamos como
-        conseguir el id del sitio de Google Analytics.</p>
+    <p>Para conseguir el código de seguimiento de Google Analytics:
+        <ol>
+            <li>
+                Inicia sesión en <a href= "https://analytics.google.com/">Google Analytics</a>
+            </li>
+            <li>
+                Despliega la pestaña de <strong>Todos los datos de sitios web</strong>.
+                En la ventana que se muestra verás tu ID.
+            </li>
+            <li>
+                Para copiar tu ID, haz click en <strong>Administrar</strong> 
+                y en la nueva ventana en <strong>Configuración de la Propiedad</strong>
+            </li>
+            <li>
+                Desde aquí podrás copiar tu <strong>ID de seguimiento</strong>
+            </li>
+        </ol>
+    </p>
 <?php
 }
 
 function cv_ayuda_faq() {
     ?>
     <ul>
-        <li>¿Google Analytics es gratis?</li>
-        <li>¿Este plugin puede hacer que mi sitio sea lento?</li>
+        <li><h5>¿Para que sirve Google Analytics?</h5>
+        Es una funcionalidad para medir el tráfico en las webs y de estadísticas de sitios web
+        </li>
+        <li><h5>¿Es gratis conseguid el código ID?</h5>
+        Si, solo necesitas una cuenta de Google para iniciar sesión
+        </li>
+        <li><h5>¿Es legal usar Google Analytics en Europa?</h5>
+        Si, solo Austria y Francia han vetado su uso por incumplimiento del RGPD
+        </li>
+        <li><h5>¿Ralentizará el plugin la carga de mi web?</h5>
+        El rendimiento de su página no se verá afectado, con la posible excepción de la primera carga de la página después de haber agregado el código de seguimiento. 
+        Esta primera página vista llama a JavaScript en los servidores de Google, lo que puede llevar un poco más de tiempo que la carga de una página normal.
+        </li>
     </ul>
 <?php
 }
@@ -78,7 +122,7 @@ function cv_genera_pagina() {
 
             <?php wp_nonce_field('token_ga'); ?>
 
-            Código de Google Analytic:
+            ID de Google Analytic:
             <input type="text" name="codigo_ga"
                    value="<?php echo esc_html($codigo_ga);
                    ?>"/>
@@ -110,21 +154,4 @@ function cv_guardar_ga() {
     exit;
 }
 
-//Agregar la funcionalidad a web en el header
-add_action( 'wp_head', 'agregar_ga' );
-function cv_agregar_ga() {
-    $codigo_ga = get_option( 'cv_ga_cuenta' ) ;
-    echo "
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
-        i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
-        a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
-        m.parentNode.insertBefore(a,m)})(window,document,'script',
-        'https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-0000000-1', 'auto');
-        ga('send', 'pageview');
-    </script>";
-}
 
